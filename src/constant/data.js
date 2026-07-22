535c384fd8d911d311ed6979df2831b01306ecc6
Michael Martinez <mirima92@gmail.com>
2026-07-21 23:26:57 -0400

    add content files and styles

---
+++ src/constant/data.js
@@ -0,0 +1,213 @@
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiFigmaFill,
+  RiCodeFill,
+  RiAccessibilityFill,
+  RiFlashlightFill,
+  RiEyeLine,
+  RiMailLine,
+  RiDownloadLine
+} from "@remixicon/react";
+
+export const socialLink = [
+  {
+    id: 1,
+    icon: RiGithubFill,
+  },
+  {
+    id: 2,
+    icon: RiLinkedinBoxFill,
+  },
+];
+
+export const navItems = [
+  {
+    id: 1,
+    label: "Home",
+    href: "#",
+  },
+  {
+    id: 2,
+    label: "About Me",
+    href: "#about",
+  },
+  {
+    id: 3,
+    label: "Projects",
+    href: "#projects",
+  },
+  {
+    id: 4,
+    label: "Contact Me",
+    href: "#contact",
+  },
+];
+
+export const heroItems = [
+  {
+    id: 1,
+    icon: RiFigmaFill,
+    subtitle: "UX/UI Design",
+    text: "Figma, Wireframing, Prototyping",
+  },
+  {
+    id: 2,
+    icon: RiCodeFill,
+    subtitle: "Development",
+    text: "React, Next.js, Astro, Tailwind CSS, Motion",
+  },
+  {
+    id: 3,
+    icon: RiAccessibilityFill,
+    subtitle: "Accessibility Testing",
+    text: "WCAG 2.2, WAI-ARIA, ANDI, Screen Readers",
+  },
+  {
+    id: 4,
+    icon: RiFlashlightFill,
+    subtitle: "Quality & Optimization",
+    text: "SEO, JIRA, Playwright, Google Analytics",
+  },
+];
+
+export const heroBtns = [
+  {
+    id: 1,
+    icon: RiEyeLine,
+    label: "View My Work",
+  },
+  {
+    id: 2,
+    icon: RiMailLine,
+    label: "Get In Touch",
+  },
+  {
+    id: 3,
+    icon: RiDownloadLine,
+    label: "Download Resume",
+  },
+];
+
+export const aboutItems = [
+  {
+    id: 1,
+    icon: "/images/icon-1.svg",
+    label: "React",
+  },
+  {
+    id: 2,
+    icon: "/images/icon-2.svg",
+    label: "Next.js",
+  },
+  {
+    id: 3,
+    icon: "/images/icon-3.svg",
+    label: "JavaScript",
+  },
+  {
+    id: 4,
+    icon: "/images/icon-4.svg",
+    label: "TypeScript",
+  },
+  {
+    id: 5,
+    icon: "/images/icon-6.svg",
+    label: "Tailwind CSS",
+  },
+  {
+    id: 6,
+    icon: "/images/icon-8.svg",
+    label: "Astro",
+  },
+  {
+    id: 7,
+    icon: "/images/icon-14.svg",
+    label: "Github",
+  },
+  {
+    id: 8,
+    icon: "/images/icon-15.svg",
+    label: "Sanity",
+  },
+  {
+    id: 9,
+    icon: "/images/icon-17.svg",
+    label: "Figma",
+  },
+];
+
+export const timelineItems = [
+  {
+    id: 1,
+    title: "Senior Website Accessibility Specialist",
+    list: ["WebFirst", "Remote", "Full-Time"],
+    time: "June 2023 - Present",
+  },
+  {
+    id: 2,
+    title: "Senior QA Analyst/ Web Accessibility Specialist",
+    list: ["Pace Communications", "Greensboro, NC, USA", "Full-Time"],
+    time: "July 2021 - June 2023",
+  },
+  {
+    id: 3,
+    title: "QA Analyst",
+    list: ["Pace Communications", "Greensboro, NC, USA", "Full-Time"],
+    time: "February 2019 - June 2021",
+  },
+  {
+    id: 4,
+    title: "Marketing & Graphics Associate",
+    list: ["Advanced Gourmet", "McCleansville, NC, USA", "Full-Time"],
+    time: "October 2018 - February 2019",
+  },
+  {
+    id: 5,
+    title: "Quality Assurance Consultant",
+    list: ["Orasi Software", "Greensboro, NC, USA", "Full-Time"],
+    time: "January 2016 - April 2018",
+  },
+];
+
+export const projectsItems = [
+  {
+    id: 1,
+    img: "/images/portfolio-1.png",
+    title: "Creative Business Landing Page",
+    text: "Creative landing page for an educational business",
+    tags: ["React", "TypeScript", "Tailwind CSS", "Motion"],
+    link: "https://flashmaster-mm.netlify.app/",
+  },
+  {
+    id: 2,
+    img: "/images/portfolio-2.png",
+    title: "Prototype Portfolio",
+    text: "Previous version of my existing portfolio",
+    tags: ["React", "Tailwind CSS"],
+    link: "https://prototype-portfolio-mm.netlify.app/",
+  }
+];
+
+export const statsItems = [
+  {
+    id: 1,
+    value: "10+",
+    text: "Happy Clients",
+  },
+  {
+    id: 2,
+    value: "100%",
+    text: "Satisfaction Rate",
+  },
+  {
+    id: 3,
+    value: "8+",
+    text: "Years Experience",
+  },
+  {
+    id: 4,
+    value: "AA",
+    text: "WCAG Conformance",
+  },
+];
